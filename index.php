ac831460f4fb942fddce1c1ab161a11de162aa1f
Nadia <ndianrhdyh@gmail.com>
2025-11-20 11:40:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,44 @@
-<?php include('db.php'); ?>
+<?php include 'koneksi.php'; ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>CRUD Sederhana</title>
+    <title>Data Buku</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h2>Data Mahasiswa</h2>
-    <a href="add.php" class="tambah-data">+ Tambah Data</a>
-    <br><br>
-    <table border="1" cellpadding="8">
-        <tr>
-            <th>No</th>
-            <th>Nama</th>
-            <th>NIM</th>
-            <th>Aksi</th>
-        </tr>
-        <?php
-        $no = 1;
-        $result = mysqli_query($conn, "SELECT * FROM mahasiswa");
-        while($row = mysqli_fetch_assoc($result)){
-            echo "<tr>
-                <td>$no</td>
-                <td>{$row['nama']}</td>
-                <td>{$row['nim']}</td>
-                <td>
-                    <a href='edit.php?id={$row['id']}'>Edit</a> |
-                    <a href='delete.php?id={$row['id']}' onclick='return confirm(\"Hapus data?\")'>Hapus</a>
-                </td>
-            </tr>";
-            $no++;
-        }
-        ?>
-    </table>
+
+<h2>Data Buku Perpustakaan</h2>
+<a href="tambah.php">+ Tambah Buku</a>
+<br><br>
+
+<table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+        <th>ID</th>
+        <th>Judul</th>
+        <th>Penulis</th>
+        <th>Penerbit</th>
+        <th>Tahun</th>
+        <th>Aksi</th>
+    </tr>
+
+    <?php
+    $data = mysqli_query($koneksi, "SELECT * FROM buku");
+    while($row = mysqli_fetch_array($data)){
+    ?>
+    <tr>
+        <td><?= $row['id']; ?></td>
+        <td><?= $row['judul']; ?></td>
+        <td><?= $row['penulis']; ?></td>
+        <td><?= $row['penerbit']; ?></td>
+        <td><?= $row['tahun']; ?></td>
+        <td>
+            <a href="edit.php?id=<?= $row['id']; ?>">Edit</a> |
+            <a href="hapus.php?id=<?= $row['id']; ?>" onclick="return confirm('Hapus data?')">Hapus</a>
+        </td>
+    </tr>
+    <?php } ?>
+
+</table>
 
 </body>
 </html>
