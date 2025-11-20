c265a362ddf24623b3927022c55082f4c9df37a9
Nadia <ndianrhdyh@gmail.com>
2025-11-20 11:39:50 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,29 +1,38 @@
-<?php include('db.php'); ?>
-<?php
+<?php 
+include 'koneksi.php';
 $id = $_GET['id'];
-$data = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM mahasiswa WHERE id=$id"));
+$data = mysqli_query($koneksi, "SELECT * FROM buku WHERE id=$id");
+$row = mysqli_fetch_assoc($data);
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-<title>Edit Data</title>
-<link rel="stylesheet" href="style.css">
-
+    <title>Edit Buku</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h2>Edit Data Mahasiswa</h2>
-<form method="post" action="">
-    Nama: <input type="text" name="nama" value="<?= $data['nama'] ?>"><br><br>
-    NIM: <input type="text" name="nim" value="<?= $data['nim'] ?>"><br><br>
-    <input type="submit" name="update" value="Update">
+
+<h2>Edit Buku</h2>
+
+<form method="POST">
+    Judul: <input type="text" name="judul" value="<?= $row['judul']; ?>" required><br><br>
+    Penulis: <input type="text" name="penulis" value="<?= $row['penulis']; ?>" required><br><br>
+    Penerbit: <input type="text" name="penerbit" value="<?= $row['penerbit']; ?>" required><br><br>
+    Tahun: <input type="number" name="tahun" value="<?= $row['tahun']; ?>" required><br><br>
+    <button type="submit" name="update">Update</button>
 </form>
 
 <?php
 if(isset($_POST['update'])){
-    $nama = $_POST['nama'];
-    $nim = $_POST['nim'];
-    mysqli_query($conn, "UPDATE mahasiswa SET nama='$nama', nim='$nim' WHERE id=$id");
-    header("Location: index.php");
+    mysqli_query($koneksi, "UPDATE buku SET 
+        judul='$_POST[judul]',
+        penulis='$_POST[penulis]',
+        penerbit='$_POST[penerbit]',
+        tahun='$_POST[tahun]'
+        WHERE id=$id
+    ");
+    header("location:index.php");
 }
 ?>
 
